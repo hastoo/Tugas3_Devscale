cbc3123d7abfe107a96b87a8d90d4a28a08187c6
hastoo <rahmahanitog@gmail.com>
2024-01-25 20:03:50 +0700

    adding typed and cofetti

--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,40 @@ window.onscroll = function () {
   }
 };
 // Navbar
+
+// Typed
+import Typed from "typed.js";
+
+const typed = new Typed("#description", {
+  strings: ["Saya suka Programing", "Saya suka belajar hal-hal baru", "Saya suka dengan teknologi"],
+  typeSpeed: 50,
+  loop: true,
+  backDelay: 2000,
+  cursorChar: "_",
+});
+
+// Typed
+
+// Confeti
+import confetti from "canvas-confetti";
+const myCanvas = document.createElement("canvas");
+myCanvas.style.width = "100vw";
+myCanvas.style.height = "100vh";
+myCanvas.style.position = "fixed";
+myCanvas.style.inset = 0;
+myCanvas.style.zIndex = -10;
+document.body.append(myCanvas);
+
+const confettiBtn = document.getElementById("hire");
+
+confettiBtn.addEventListener("click", function () {
+  const myConfetti = confetti.create(myCanvas, {
+    resize: true,
+    useWorker: true,
+  });
+  myConfetti({
+    particleCount: 100,
+    spread: 160,
+  });
+});
+// Confeti
